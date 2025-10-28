79afbb0ec3950a12c1134b2689366d092461244a
Anthony <deflexable@gmail.com>
2025-10-28 17:15:59 +0100

    added segment-binary library

--- readable/index.js
+++ readable/index.js
@@ -1,5 +1,5 @@
-
 const { Transform } = require('stream');
+const { desegmentBinary } = require('segment-binary');
 
 class ReadableBit extends Transform {
     constructor(opts) {
@@ -12,11 +12,11 @@ class ReadableBit extends Transform {
             this.leftOvers = this.leftOvers ? Buffer.concat([this.leftOvers, chunk]) : chunk;
             let buf;
 
-            const stream_bit = desegmentBuffer(this.leftOvers);
+            const stream_bit = desegmentBinary(this.leftOvers);
             if (stream_bit) {
-                const [bits, remainder] = stream_bit;
+                const { blocks, remainder } = stream_bit;
                 this.leftOvers = remainder;
-                buf = bits;
+                buf = blocks;
             }
             if (buf) {
                 buf.forEach(e => {
@@ -36,39 +36,6 @@ class ReadableBit extends Transform {
             callback();
         }, 0);
     }
-}
-
-/**
- * @type {( buf: Buffer ) => null | [Buffer, Buffer]}
- */
-const desegmentBuffer = (buf) => {
-    let offset = 0, thisSegment;
-    const blocks = [];
-
-    while ((thisSegment = readVariableLength(buf, offset)) !== null) {
-        const { byteSize, byteOffset } = thisSegment;
-        const binary = buf.subarray(offset = byteOffset, offset += byteSize);
-        blocks.push(binary);
-    }
-
-    if (!blocks.length) return null;
-    return [blocks, buf.subarray(offset, buf.length)];
-}
-
-function readVariableLength(buffer, offset) {
-    if (buffer.length - offset === 0) return null;
-    const unsignIntSize = buffer.readUIntBE(offset, 1);
-
-    if (++offset + unsignIntSize > buffer.length) return null;
-
-    const byteSize = buffer.readUIntBE(offset, unsignIntSize);
-
-    if (offset + unsignIntSize + byteSize > buffer.length) return null;
-
-    return {
-        byteOffset: offset + unsignIntSize,
-        byteSize
-    };
 }
 
 module.exports = {
